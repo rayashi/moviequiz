b589b2d025007be47daf226252e3813459fe3caa
Humberto Dib Rayashi <rayashi@gmail.com>
2019-04-19 09:29:07 -0300

    Loading json files fo quizes and options

--- MovieQuiz/GameViewController.swift
+++ MovieQuiz/GameViewController.swift
@@ -8,11 +8,25 @@ class GameViewController: UIViewController {
     @IBOutlet var btOptions: [UIButton]!
     @IBOutlet weak var imageQuiz: UIImageView!
     
+    var quizManager = QuizManger()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        quizManager = QuizManger()
+        getNewRound()
+    }
+    
+    func getNewRound() {
+        let round = quizManager.generateRandomQuiz()
+        for i in 0..<round.options.count {
+            btOptions[i].setTitle(round.options[i].name, for: .normal)
+        }
+    }
+    
     @IBAction func checkAnswer(_ sender: UIButton) {
     }
     
---
+++ MovieQuiz/QuizManager.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+typealias Round = (quiz: Quiz, options: [Option])
+
+class QuizManger {
+    var quizes: [Quiz] = []
+    var options: [Option] = []
+    var score: Int = 0
+    var round: Round?
+    
+    init() {
+        let quizesURL = Bundle.main.url(forResource: "quizes", withExtension: "json")!
+        let optionsURL = Bundle.main.url(forResource: "options", withExtension: "json")!
+        do {
+            let quizesData = try Data(contentsOf: quizesURL)
+            quizes = try JSONDecoder().decode([Quiz].self, from: quizesData)
+            let optionsData = try Data(contentsOf: optionsURL)
+            options = try JSONDecoder().decode([Option].self, from: optionsData)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func generateRandomQuiz() -> Round {
+        let quizIndex = Int(arc4random_uniform(UInt32(quizes.count)))
+        let quiz = quizes[quizIndex]
+        var optionsIndex: Set<Int> = [quizIndex]
+        while optionsIndex.count < 4 {
+            optionsIndex.insert(Int(arc4random_uniform(UInt32(quizes.count))))
+        }
+        let opts = optionsIndex.map{options[$0]}
+        return (quiz, opts)
+    }
+    
+    func validateAnswer(_ answer: String) {
+        guard let round = round else { return }
+        answer == round.quiz.name ? score += 1 : nil
+    }
+}
+
+
+struct Quiz: Codable {
+    let name: String
+    let number: Int
+}
+
+struct Option: Codable {
+    let name: String
+}
